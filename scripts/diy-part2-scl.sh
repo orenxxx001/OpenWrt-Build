902cd3ecbb49ea21627765dc966b32e5699ced73
orenxxx001 <33869335+orenxxx001@users.noreply.github.com>
2022-03-12 13:32:21 +0800

    Update diy-part2-scl.sh

--- scripts/diy-part2-scl.sh
+++ scripts/diy-part2-scl.sh
@@ -10,8 +10,20 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Modify default IP
-sed -i 's/192.168.1.1/192.168.66.1/g' package/base-files/files/bin/config_generate
+# 更改默认密码
+sed -i 's/root::0:0:99999:7:::/root:$1$MhPcOOTE$DOOyDUwKjP9xnoSfaczsk.:19058:0:99999:7:::/g' package/base-files/files/etc/shadow
+
+# 修改默认IP
+sed -i 's/192.168.1.1/192.168.88.8/g' package/base-files/files/bin/config_generate
+
+# 修改hostname
+sed -i 's/OpenWrt/XinV-2.0/g' package/base-files/files/bin/config_generate
+
+# Modify the version number版本号里显示一个自己的名字（AutoBuild $(TZ=UTC-8 date "+%Y.%m.%d") @ 这些都是后增加的）
+sed -i 's/OpenWrt /Build $(TZ=UTC-8 date "+%Y.%m.%d") @ XinV-2.0 /g' package/lean/default-settings/files/zzz-default-settings
+
+# 修改主机名字，把XinV-2.0修改你喜欢的就行（不能纯数字或者使用中文）
+sed -i '/uci commit system/i\uci set system.@system[0].hostname='XinV-2.0'' package/lean/default-settings/files/zzz-default-settings
 
 # Clone community packages to package/community
 #mkdir package/community
@@ -56,12 +68,7 @@ git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/lean/luci-
 
 # 取消bootstrap为默认主题 & Add luci-theme-atmaterial_new
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
-git clone https://github.com/aswifi/luci-theme-atmaterial_new.git package/lean/luci-theme-atmaterial_new
-#rm -rf package/lean/luci-theme-bootstrap
 
-# Mod zzz-default-settings & Modify the version number & Delete default password
-pushd package/lean/default-settings/files
-sed -i '/http/d' zzz-default-settings
-export orig_version="$(cat "zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')"
-sed -i "s/OpenWrt /Redmi-AC2100 build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" zzz-default-settings
-sed -i "/CYXluq4wUazHjmCDBCqXF/d" zzz-default-settings
+# 替换默认主题为 luci-theme-darkmatter
+# sed -i 's/更改前的信息/更改后的信息/g' ./要修改的文件的目录（可以用本地查看）
+sed -i 's/luci-theme-bootstrap/luci-theme-darkmatter/g' feeds/luci/collections/luci/Makefile
